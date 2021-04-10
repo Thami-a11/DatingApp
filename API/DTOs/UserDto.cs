3d229064a2b5c9eb2196a80d662dc198fb9235b0
thami <gg>
2021-04-10 16:33:16 +0200

    End of Section 4

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class UserDto
+    {
+         //[Required]
+        public string Username { get; set; }
+        
+        //[Required]
+        public string Token { get; set; }
+    }
+}
