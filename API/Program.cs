4b575bf126813d852c5980ab10015c1e4c3ce6c8
thami <gg>
2021-05-26 19:01:31 +0200

    Section 19_

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ namespace API {
                 var context = services.GetRequiredService<DataContext> ();
                 var userManager = services.GetRequiredService<UserManager<AppUser>> ();
                 var roleManager = services.GetRequiredService<RoleManager<AppRole>> ();
-                await context.Database.MigrateAsync ();
+                await context.Database.MigrateAsync();
                 await Seed.SeedUsers (userManager, roleManager);
             } catch (Exception ex) {
                 var logger = services.GetRequiredService<ILogger<Program>> ();
